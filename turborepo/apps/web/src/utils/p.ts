def0f014859d448cfae6d1cca27dd5c09703dc27
Andrew Ross <andrew.ross@takeda.com>
2025-06-16 11:38:56 -0500

    fixed auth/core dep issue with apps/web deployment

---
+++ turborepo/apps/web/src/utils/p.ts
@@ -0,0 +1,30 @@
+import * as dotenv from "dotenv";
+import { default as prisma } from "@/lib/prisma";
+
+dotenv.config();
+
+if (process.argv[3] === "users") {
+  (async () => {
+    try {
+      prisma.$connect();
+    } catch (err) {
+      if (err instanceof Error) {
+        console.error(err.message);
+      } else console.error(JSON.stringify(err, null, 2));
+    } finally {
+      const data = await prisma.user.findMany({
+        include: {
+          accounts: true,
+          profile: true,
+          sessions: true,
+          settings: true,
+          _count: true
+        }
+      });
+      return data;
+    }
+  })().then(res => {
+    console.log(JSON.stringify(res, null, 2));
+    prisma.$disconnect();
+  });
+}
