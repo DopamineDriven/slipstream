9f9061f2e024d409b60c77832002eddce12d197d
Andrew Ross <andrew.ross@takeda.com>
2025-09-24 19:30:38 -0500

    up

--- aws-bucket-cors.sh
+++ aws-bucket-cors.sh
@@ -15,6 +15,14 @@ DEV_NEW=https://dev.chat.aicoalesce.com
 STG_NEW=https://stg.chat.aicoalesce.com
 PY_NEW=https://py.aicoalesce.com
 WS_NEW=https://ws.aicoalesce.com
+ADOBE_PDF=https://pdf-services.adobe.io
+ADOBE_PDF_UE1=https://pdf-services-ue1.adobe.io
+ADOBE_PDF_EW1=https://pdf-services-ew1.adobe.io
+ADOBE_PDF_COM=https://pdf-services.adobe.com
+ADOBE_IMS_NA1=https://ims-na1.adobelogin.com
+ADOBE_IMS_EU1=https://ims-eu1.adobelogin.com
+ADOBE_DC_IO=https://dc-api.adobe.io
+ADOBE_DC_COM=https://dc-api.adobe.com
 
 CFG=$(jq -n \
   --arg local "$LOCAL" \
@@ -28,6 +36,14 @@ CFG=$(jq -n \
   --arg stg_new "$STG_NEW" \
   --arg py_new "$PY_NEW" \
   --arg ws_new "$WS_NEW" \
+  --arg adobe_pdf "$ADOBE_PDF" \
+  --arg adobe_pdf_ue1 "$ADOBE_PDF_UE1" \
+  --arg adobe_pdf_ew1 "$ADOBE_PDF_EW1" \
+  --arg adobe_pdf_com "$ADOBE_PDF_COM" \
+  --arg adobe_ims_na1 "$ADOBE_IMS_NA1" \
+  --arg adobe_ims_eu1 "$ADOBE_IMS_EU1" \
+  --arg adobe_dc_io "$ADOBE_DC_IO" \
+  --arg adobe_dc_com "$ADOBE_DC_COM" \
 '{
   CORSRules: [
     {
@@ -35,7 +51,10 @@ CFG=$(jq -n \
         $local, 
         $local_py,
         $root_old, $dev_old, $stg_old, $py_old,
-        $root_new, $dev_new, $stg_new, $py_new, $ws_new
+        $root_new, $dev_new, $stg_new, $py_new, $ws_new,
+        $adobe_pdf, $adobe_pdf_ue1, $adobe_pdf_ew1, $adobe_pdf_com,
+        $adobe_ims_na1, $adobe_ims_eu1,
+        $adobe_dc_io, $adobe_dc_com
       ],
       AllowedMethods: ["GET","HEAD","PUT","POST","DELETE"],
       AllowedHeaders: ["*"],
